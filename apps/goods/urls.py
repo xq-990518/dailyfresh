0fadde6fdab50a74ad960e3e796fee7af269a7f3
xq-990518 <11410121298@qq.com>
2020-04-01 16:55:27 +0800

    新增商品全文搜索框架,增加更多变量

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from goods.views import IndexView, DetailView, GoodsListView
+from goods.views import IndexView, DetailView, GoodsListView, MySeachView
 
 urlpatterns = [
     url(r'^index$', IndexView.as_view(), name='index'),  # 首页
     url(r'^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name='detail'),  # 详情
     url(r'^list/(?P<type_id>\d+)/(?P<page>\d+)$', GoodsListView.as_view(), name='list'),  # 列表页
-
+    url(r'^search/', MySeachView(), name='haystack_search'),
 ]
--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -195,8 +195,9 @@ CUSTOM_STORAGE_OPTIONS = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         # 使用whoosh引擎
-        # 'ENGINE': 'haystack.backends.whoosh_con_backend.WHooshEngine',
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        # 使用jieba分词
+        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
         # 索引文件路径
         'PATH': os.path.join(BASE_DIR, 'whoosh_index')
     }
@@ -204,3 +205,6 @@ HAYSTACK_CONNECTIONS = {
 
 # 当添加、修改、删除数据时,自动生成索引
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+# 指定搜索结果每页显示的条数
+HAYSTACK_SEARCH_RESULTS_PER_PAGE=1
